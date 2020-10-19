1144a5b8b925a568e371b59f18086245c42b7c8c
vladimir_ubuntu <dyba.vladimir@gmail.com>
2020-10-19 20:21:13 +0300

    everything works

--- models/database.go
+++ models/database.go
@@ -11,13 +11,8 @@ import (
 var DB *gorm.DB
 
 //ConnectDB ...
-<<<<<<< HEAD
 func ConnectDB(dialect string, params string) {
 	db, err := gorm.Open(dialect, params)
-=======
-func ConnectDB() {
-	db, err := gorm.Open("postgres", "host= port= user= dbname= password= sslmode=disable")
->>>>>>> 81f14c17c98ebab3d7f31e7daa6056ead0e538fe
 	if err != nil {
 		panic("Не удалось подключиться к базе данных")
 	}

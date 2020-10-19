81f14c17c98ebab3d7f31e7daa6056ead0e538fe
dybadotcode <46761665+dybadotcode@users.noreply.github.com>
2020-10-19 17:36:29 +0300

    Update database.go

--- models/database.go
+++ models/database.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 
 //ConnectDB ...
 func ConnectDB() {
-	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=demo password=q sslmode=disable")
+	db, err := gorm.Open("postgres", "host= port= user= dbname= password= sslmode=disable")
 	if err != nil {
 		panic("Не удалось подключиться к базе данных")
 	}
